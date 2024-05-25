6ada7861c8c177b0dcb2e325cd11cb975e7696ac
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-05-26 00:41:17 +0530

    Update : Generate Report button

--- extension/content.js
+++ extension/content.js
@@ -1,23 +1,25 @@
 // Content script (content.js)
-// This script runs on web pages to interact with the DOM and extract code.
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "extractCode") {
-    const code = extractCodeFromDiv();
-    sendResponse({ code: code });
-  }
+function addButton() {
+  const element = document.createElement("button");
+  element.innerHTML = "Generate Report";
+  element.style.cssText =
+    "background-color: #222222; color: #007CFF; padding: 5px 10px; text-align: center; border-radius: 5px; text-decoration: none; display: inline-block; font-size: 14px; margin: 0px 5px; cursor: pointer; font-weight: 600;";
+  document.getElementById("ide-top-btns").appendChild(element);
+  console.log("Button appended");
+}
+
+console.log("Content script loaded");
+chrome.storage.local.get(["key"]).then((result) => {
+  console.log("Value is client " + result.key);
 });
 
-function extractCodeFromDiv() {
-  const codeDiv = document.querySelector(".view-lines");
-  if (!codeDiv) {
-    return null;
+// Wait for the DOM to be fully loaded before adding the button
+// add do while loop to check if the element is loaded
+
+let checkExist = setInterval(function () {
+  if (document.getElementById("ide-top-btns")) {
+    addButton();
+    clearInterval(checkExist);
   }
-  let code = "";
-  const lines = codeDiv.querySelectorAll(".view-line");
-  lines.forEach((line) => {
-    const text = line.textContent.trim();
-    code += text + "\n";
-  });
-  return code.trim();
-}
+}, 100); // check every 100ms
